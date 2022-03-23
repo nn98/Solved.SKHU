7f0743726300b843bf857e065698fcc13bf07fbc
hyunboklee <gusqhrzkq@naver.com>
2022-03-23 13:33:10 +0900

    #2 main menu 와 footer 경계선 이미지 태그 추가

--- front-main_page/src/footer.js
+++ front-main_page/src/footer.js
@@ -1,10 +1,16 @@
 import Skhu_log from './image/skhu_log.png'
 
+import Main_Bottom_Line from './image/main_bottom_line.png'
+
 const Footer = () => {
   return (
     <>
       <div className="footer">
-        <hr style={{ width: '98%', background: '#000' }} />
+        <img
+          src={Main_Bottom_Line}
+          style={{ width: '90%', margin: '0 5%' }}
+          alt="profile"
+        />
         <div style={{ float: 'right', padding: '0.5% 2% 0 0' }}>
           <img src={Skhu_log} style={{ width: '100%' }} alt="profile" />
         </div>
--- front-main_page/src/mainMenu.js
+++ front-main_page/src/mainMenu.js
@@ -1,6 +1,7 @@
 // import { Link } from 'react-router-dom'
 
-import Logo from "./image/logo.png";
+import Logo from './image/logo.png'
+import Main_Top_Line from './image/main_top_line.png'
 
 const MainMenu = (props) => {
   return (
@@ -12,7 +13,7 @@ const MainMenu = (props) => {
               <td>
                 <img
                   src={Logo}
-                  style={{ float: "left", marginLeft: "10%" }}
+                  style={{ float: 'left', marginLeft: '10%' }}
                   alt="profile"
                 />
               </td>
@@ -31,10 +32,14 @@ const MainMenu = (props) => {
             </tr>
           </thead>
         </table>
-        <hr style={{ width: "98%", background: "#000" }} />
+        <img
+          src={Main_Top_Line}
+          style={{ width: '90%', margin: '0 5%' }}
+          alt="profile"
+        />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default MainMenu;
+export default MainMenu
