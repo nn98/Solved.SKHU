6011fccf3bcb3fbca383d36b7096e2c81205760b
hyunboklee <gusqhrzkq@naver.com>
2022-04-05 16:13:42 +0900

    #3 user의 json 형식 파일을 직접 만들고 user.css와 userPage.js 설정

--- front-main_page/src/pages/userPage.js
+++ front-main_page/src/pages/userPage.js
@@ -1,22 +1,82 @@
-import React from "react";
+import React from 'react'
+import './user.css'
+import usersJ from './users.json'
 
-export const userPage = () => {
-  const iframePart = () => {
-    return {
-      __html:
-        '<iframe src="https://solved.ac/profile/asb0313/solved?sort=level&direction=desc&page=1" style="width:100%; height: 100vh" frameborder="0"/>',
-    };
-  };
+const UserPage = () => {
+  const save = usersJ
 
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-      dangerouslySetInnerHTML={iframePart()}
-    />
-  );
-};
+    <div className="user">
+      <div>
+        <h1>{save.Id}</h1>
+      </div>
+      <div>
+        <h1>{save.user_tear}</h1>
+      </div>
+      <div>
+        <div className="problem">
+          {save.user_problems.map((problem, index) => (
+            <a
+              href={'https://www.acmicpc.net/problem/' + problem}
+              style={{ textDecorationLine: 'none', color: '#000' }}
+            >
+              {problem} {''}
+            </a>
+          ))}
+        </div>
+        <table className="tear">
+          <thead>
+            <tr>
+              <th
+                colSpan="3"
+                dangerouslySetInnerHTML={{ __html: save.solved_tear_chart }}
+                style={{ width: '100%' }}
+              >
+                {/* 그냥 넣음 ㅂㄷㅂㄷ */}
+              </th>
+            </tr>
+          </thead>
 
-export default userPage;
+          {save.solved_tear.map((BigTears) => (
+            <tbody key={BigTears.big_tear}>
+              <tr>
+                <td colSpan="3">{BigTears.big_tear}</td>
+              </tr>
+
+              {BigTears.type.map((tear) => (
+                <tr key={tear.tear}>
+                  <td>{tear.tear}</td>
+                  <td>{tear.problem}</td>
+                  <td>{tear.EXP}</td>
+                </tr>
+              ))}
+            </tbody>
+          ))}
+        </table>
+
+        <table className="tag">
+          <thead>
+            <tr>
+              <th
+                colSpan="3"
+                dangerouslySetInnerHTML={{ __html: save.solved_tag_chart }}
+                style={{ width: '100%' }}
+              ></th>
+            </tr>
+          </thead>
+          <tbody>
+            {save.solved_tag.map((tags) => (
+              <tr key={tags.name}>
+                <td>{tags.name}</td>
+                <td>{tags.problem}</td>
+                <td>{tags.EXP}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default UserPage
