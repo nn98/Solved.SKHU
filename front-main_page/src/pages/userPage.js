e0a5fa8996f4364f8f32e407cdc9bf1dd222c0c1
Styx2021 <leeys6759@gmail.com>
2022-04-06 17:26:08 +0900

    #3 유저페이지 UI 틀 구현

--- front-main_page/src/pages/userPage.js
+++ front-main_page/src/pages/userPage.js
@@ -12,60 +12,62 @@ const UserPage = () => {
       </h1>
       <h1>{save.user_tear}</h1>
 
-      <div className="tearTable">
-        <div
-          dangerouslySetInnerHTML={{ __html: save.solved_tear_chart }}
-          style={{ width: "50%", float: "left" }}
-        ></div>
-        <table>
-          <thead></thead>
-          {save.solved_tear.map((BigTears) => (
-            <tbody key={BigTears.big_tear}>
-              <tr>
-                <td colSpan="3">{BigTears.big_tear}</td>
-              </tr>
+      <div className="use">
+        <div className="tearTable">
+          <div
+            dangerouslySetInnerHTML={{ __html: save.solved_tear_chart }}
+            style={{ width: "50%", float: "left" }}
+          ></div>
+          <table>
+            <thead></thead>
+            {save.solved_tear.map((BigTears) => (
+              <tbody key={BigTears.big_tear}>
+                <tr>
+                  <td colSpan="3">{BigTears.big_tear}</td>
+                </tr>
 
-              {BigTears.type.map((tear) => (
-                <tr key={tear.tear}>
-                  <td>{tear.tear}</td>
-                  <td>{tear.problem}</td>
-                  <td>{tear.EXP}</td>
+                {BigTears.type.map((tear) => (
+                  <tr key={tear.tear}>
+                    <td>{tear.tear}</td>
+                    <td>{tear.problem}</td>
+                    <td>{tear.EXP}</td>
+                  </tr>
+                ))}
+              </tbody>
+            ))}
+          </table>
+        </div>
+
+        <div className="problem">
+          {save.user_problems.map((problem, index) => (
+            <a
+              key={index}
+              href={"https://www.acmicpc.net/problem/" + problem}
+              style={{ textDecorationLine: "none", color: "#000" }}
+            >
+              {problem} {""}
+            </a>
+          ))}
+        </div>
+
+        <div className="tagTable">
+          <div
+            dangerouslySetInnerHTML={{ __html: save.solved_tag_chart }}
+            style={{ width: "50%", float: "left" }}
+          ></div>
+          <table>
+            <thead></thead>
+            <tbody>
+              {save.solved_tag.map((tags) => (
+                <tr key={tags.name}>
+                  <td>{tags.name}</td>
+                  <td>{tags.problem}</td>
+                  <td>{tags.EXP}</td>
                 </tr>
               ))}
             </tbody>
-          ))}
-        </table>
-      </div>
-
-      <div className="tagTable">
-        <div
-          dangerouslySetInnerHTML={{ __html: save.solved_tag_chart }}
-          style={{ width: "40%", float: "left" }}
-        ></div>
-        <table>
-          <thead></thead>
-          <tbody>
-            {save.solved_tag.map((tags) => (
-              <tr key={tags.name}>
-                <td>{tags.name}</td>
-                <td>{tags.problem}</td>
-                <td>{tags.EXP}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="problem">
-        {save.user_problems.map((problem, index) => (
-          <a
-            key={index}
-            href={"https://www.acmicpc.net/problem/" + problem}
-            style={{ textDecorationLine: "none", color: "#000" }}
-          >
-            {problem} {""}
-          </a>
-        ))}
+          </table>
+        </div>
       </div>
     </div>
   );
