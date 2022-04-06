38f69eea40c7b12ff54616f97bef8a40c57001fe
hyunboklee <gusqhrzkq@naver.com>
2022-04-06 15:41:35 +0900

    #3 setState 데이터 추가 시 기존 데이터에 덧붙이기

--- front-main_page/src/pages/QnA.js
+++ front-main_page/src/pages/QnA.js
@@ -6,6 +6,19 @@ const QnA = () => {
   const [commentAddPassword, setCommentAddPassword] = useState('')
   const [commentAddContent, setCommentAddContent] = useState('')
   const [test, setTest] = useState([])
+  const add = (props) => {
+    const body = {
+      addName: props.commentAddContent,
+      addPasseword: props.commentAddPassword,
+      addContent: props.commentAddContent,
+    }
+    // console.log(body)
+    setTest([...test, body])
+  }
+
+  useEffect(() => {
+    console.log(test)
+  }, [test])
 
   // 댓글이 제대로 등록 될때 실행(사실 props.error의 내용이 변한때마다 실행)
 
@@ -39,6 +52,13 @@ const QnA = () => {
           // }
           name="commenting"
           value="댓글 작성"
+          onClick={() => {
+            add({
+              commentAddName,
+              commentAddContent,
+              commentAddPassword,
+            })
+          }}
         >
           댓글 작성
         </button>
