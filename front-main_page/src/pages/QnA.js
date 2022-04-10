8e1ef44ccc0417135893d55650d8dcf7a1cc22a0
hyunboklee <gusqhrzkq@naver.com>
2022-04-10 16:10:53 +0900

    #17 댓글 작성시 회원의 정보가 일치하다면 댓글 추가, 회원가입 완료 시 어서오세요 메세지 출력

--- front-main_page/src/pages/QnA.js
+++ front-main_page/src/pages/QnA.js
@@ -17,6 +17,10 @@ const userCreateStyle = {
 }
 
 const QnA = () => {
+  // 회원가입 열고 닫기 창
+  const [open, setOpen] = useState(false)
+  const userHandleOpen = () => setOpen(!open)
+
   // 유저를 위한 name password
   const [createUserName, setCreateUserName] = useState('')
   const [createUserPassword, setCreateUserPassword] = useState('')
@@ -30,25 +34,23 @@ const QnA = () => {
       // 유저 정보 body에 저장
       const body = {
         name: props.createUserName,
-        Password: props.createUserPassword,
+        password: props.createUserPassword,
       }
-
+      // 유저가 1명 이상일때만
       if (users.length > 0) {
+        // 유저의 아이디와 생성할 아이디가 같으면
         for (let i = 0; i < users.length; i++) {
           if (users[i].name === props.createUserName) {
-            setCreateUserName('')
-            setCreateUserPassword('')
+            // 오류 메세지 출력
             return alert('현재 사용 중인 사용자가 있습니다.')
           }
         }
       }
-      console.log('test')
-      // createUserName과 createUserPassword 빈칸으로 만들기
-      setCreateUserName('')
-      setCreateUserPassword('')
 
       // 유저 보관함에 추가
       setUser([...users, body])
+      setOpen(!open)
+      return alert('어서오세요')
     } catch (error) {
       alert('실패하였습니다.')
       console.error(error)
@@ -68,15 +70,30 @@ const QnA = () => {
     try {
       // 먼저 댓글 받은 유저의 정보와 쓴 댓글 내용을 body에 저장
       const body = {
-        addName: props.commentAddContent,
+        addName: props.commentAddName,
         addPasseword: props.commentAddPassword,
         addContent: props.commentAddContent,
       }
 
-      console.log(comments[0])
+      // 유저가 하나도 없으면
+      if (users.length === 0) return alert('없는 사용자 입니다.')
 
-      // 댓글 보관함에 저장
-      setComments([...comments, body])
+      // 유저를 찾기 위한 반목문 실행
+      for (let i = 0; i < users.length; i++) {
+        // user의 이름과 입력받은 이름이 같으면
+        if (users[i].name === props.commentAddName) {
+          // 만약 비밀번호만 다르다면
+          if (users[i].password !== props.commentAddPassword)
+            // 오류 출력
+            return alert('비밀번호가 같지 않습니다.')
+          // 댓글 보관함에 저장
+          setComments([...comments, body])
+          return
+        }
+      }
+
+      // 오류 메세지 출력
+      return alert('사용자가 없습니다.')
     } catch (error) {
       alert('실패하였습니다.')
       console.error(error)
@@ -85,13 +102,9 @@ const QnA = () => {
 
   // 댓글보관함이 수정 될 때 마다 리로딩
   useEffect(() => {
-    console.log('users: ' + users)
-    console.log('comments: ' + comments)
+    console.log('users: ' + users.length)
+    console.log('comments: ' + comments.length)
   }, [comments, users])
-
-  // 회원가입 열고 닫기 창
-  const [open, setOpen] = useState(false)
-  const userHandleOpen = () => setOpen(!open)
 
   return (
     <div className="comments">
@@ -119,9 +132,12 @@ const QnA = () => {
                 disabled={!createUserName || !createUserPassword}
                 name="commenting"
                 value="Signup"
-                onClick={() =>
+                onClick={() => {
                   createUser({ createUserName, createUserPassword })
-                }
+                  // createUserName과 createUserPassword 빈칸으로 만들기
+                  setCreateUserName('')
+                  setCreateUserPassword('')
+                }}
               >
                 Signup
               </button>
