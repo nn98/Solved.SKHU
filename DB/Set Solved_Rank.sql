152058d5af95fba1008df7d27296d932b9404ae8
nn98 <jkllhgb@gmail.com>
2022-04-29 23:15:10 +0900

    #21 Update : DB Build/Solvedrank

--- DB/Set Solved_Rank.sql
+++ DB/Set Solved_Rank.sql
@@ -1,6 +1,6 @@
 use SWP;
 insert into Solvedrank (tier)
-values ('Bronze5'),('Bronze4'),('Bronze3'),('Bronze2'),
+values ('Unknown'),('Bronze5'),('Bronze4'),('Bronze3'),('Bronze2'),
 ('Bronze1'),('Silver5'),('Silver4'),('Silver3'),('Silver2'),
 ('Silver1'),('Gold5'),('Gold4'),('Gold3'),('Gold2'),('Gold1'),
 ('Platinum5'),('Platinum4'),('Platinum3'),('Platinum2'),
