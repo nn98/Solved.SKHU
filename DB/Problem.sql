824928f3ce96ef436abffc120ec69edb9269a130
seobkeom <ajm153123@naver.com>
2022-05-31 15:42:46 +0900

    #4 Feat : 타겟 인접 유저 정보 추출

--- DB/Problem.sql
+++ DB/Problem.sql
@@ -11,15 +11,23 @@ select ID,namekr, rate, SOLVED_RANK from Problem where ID in (select PROBLEM_ID 
 -- 성공률 하위 10개
 select ID,namekr, rate, SOLVED_RANK from Problem where ID in (select PROBLEM_ID from Solve) and namekr regexp '^[가-힇 % %]*$' order by cast(rate as signed) limit 0,10;
 
+select ID, skhurank from User order by skhurank;
+
 -- 유저 기준 위 아래 2명의(총 4명) 푼 문제 리스트
-select PROBLEM_ID ,count(PROBLEM_ID) as sum from User join Solve on User.ID = Solve.USER_ID where ID in (
+
+select PROBLEM_ID, namekr, SOLVED_RANK ,count(PROBLEM_ID) as sum from User right join Solve on User.ID = Solve.USER_ID
+join Problem on Solve.PROBLEM_ID = Problem.ID
+where User.ID in (
 select ID from User where skhurank = (select skhurank from User where ID='q9922000')+2
 union
 select ID from User where skhurank = (select skhurank from User where ID='q9922000')+1
 union
 select ID from User where skhurank = (select skhurank from User where ID='q9922000')-1
 union
-select ID from User where skhurank = (select skhurank from User where ID='q9922000')-2) group by PROBLEM_ID having count(PROBLEM_ID)>1 order by count(PROBLEM_ID) desc;
+select ID from User where skhurank = (select skhurank from User where ID='q9922000')-2)
+and PROBLEM_ID not in(select PROBLEM_ID from Solve where USER_ID = 'q9922000')
+group by PROBLEM_ID having count(PROBLEM_ID)>=1 order by count(PROBLEM_ID) desc;
+
 
 select * from Algorithm; -- O
 select * from Problem; -- O
