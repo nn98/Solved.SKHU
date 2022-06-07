bdeaa245d41c81992fd86e7e0e7551fe101bc432
Styx2021 <leeys6759@gmail.com>
2022-06-07 20:17:36 +0900

    #32 Design : 날짜에 마우스를 올렸을 때 상세 날짜가 Tooltip으로 나오도록 구현

--- front-main_page/src/pages/MUI/MaxWidthDialog.js
+++ front-main_page/src/pages/MUI/MaxWidthDialog.js
@@ -6,10 +6,30 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import "./MaxWidthDialog.css";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { styled } from "@mui/material/styles";
 
 export default function MaxWidthDialog(props) {
+  let detailDate = "";
+
   const handleClose = () => {
     props.setOpen(false);
+  };
+
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: "rgba(0, 0, 0, 0.87)",
+      color: "rgba(255, 255, 255, 0.87)",
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(18),
+    },
+  }));
+
+  const setDetailDate = (value) => {
+    detailDate = value;
+    return;
   };
 
   return (
@@ -43,12 +63,66 @@ export default function MaxWidthDialog(props) {
                           <th>코드 길이</th>
                           <th>제출한 시간</th>
                         </tr>
-                        {data.status.map((result, index) => (
-                          <React.Fragment key={index}>
-                            {index !== 0 ? (
-                              <tr
-                                dangerouslySetInnerHTML={{ __html: result }}
-                              ></tr>
+                        {data.status.map((result, _index) => (
+                          <React.Fragment key={_index}>
+                            {_index !== 0 ? (
+                              <tr>
+                                {result.map((value, index) => (
+                                  <React.Fragment key={index}>
+                                    {index === 3 ? (
+                                      value === "맞았습니다!!" ? (
+                                        <td style={{ color: "green" }}>
+                                          {value}
+                                        </td>
+                                      ) : (
+                                        <td style={{ color: "red" }}>
+                                          {value}
+                                        </td>
+                                      )
+                                    ) : index === 4 ? (
+                                      value !== "" ? (
+                                        <td>
+                                          {value}{" "}
+                                          <span style={{ color: "#CC723D" }}>
+                                            KB
+                                          </span>
+                                        </td>
+                                      ) : (
+                                        <td></td>
+                                      )
+                                    ) : index === 5 ? (
+                                      value !== "" ? (
+                                        <td>
+                                          {value}{" "}
+                                          <span style={{ color: "#CC723D" }}>
+                                            ms
+                                          </span>
+                                        </td>
+                                      ) : (
+                                        <td></td>
+                                      )
+                                    ) : index === 7 ? (
+                                      <td>
+                                        {value}{" "}
+                                        <span style={{ color: "#CC723D" }}>
+                                          B
+                                        </span>
+                                      </td>
+                                    ) : index === 8 ? (
+                                      <>{setDetailDate(value)}</>
+                                    ) : index === 9 ? (
+                                      <HtmlTooltip
+                                        title={detailDate}
+                                        placement="top"
+                                      >
+                                        <td>{value}</td>
+                                      </HtmlTooltip>
+                                    ) : (
+                                      <td>{value}</td>
+                                    )}
+                                  </React.Fragment>
+                                ))}
+                              </tr>
                             ) : null}
                           </React.Fragment>
                         ))}
