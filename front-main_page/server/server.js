49e30c814e8e037154d433fd843a454276a584cb
nn98 <jkllhgb@gmail.com>
2022-06-17 19:06:34 +0900

    #77 Fix : 배포를 위한 수정

--- front-main_page/server/server.js
+++ front-main_page/server/server.js
@@ -28,7 +28,7 @@ app.listen(port, () => {
 
 var mysql = require("mysql");
 var connection = mysql.createPool({
-  host: "3.39.230.170",
+  host: "54.180.80.37",
   user: "Project",
   password: "testing00",
   database: "SWP",
