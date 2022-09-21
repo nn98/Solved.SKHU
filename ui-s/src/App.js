e7ae856e624a05153ba2af6589b42fbf8ac75e37
hyunboklee <gusqhrzkq@naver.com>
2022-09-21 20:53:09 +0900

    #82 Feat : route 적용

--- ui-s/src/App.js
+++ ui-s/src/App.js
@@ -1,36 +1,18 @@
-import React, { useState } from "react";
-import "./App.css";
-import "./Front.css";
+import React, { useState } from 'react'
+import './App.css'
+import './Front.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Main from './Main'
+import Student from './Student'
 
 function App() {
-  const [stuStyle, setStuStyle] = useState({});
-  const [butStyle, setButStyle] = useState({});
-
-  const proClick = () => {
-    setStuStyle({ transform: "translate(50%)", transition: "1s" });
-    setButStyle({ opacity: "0", transition: "0.5s" });
-  };
-
-  const stuClick = () => {
-    setStuStyle({
-      transform: "translate(-50%)",
-      transition: "1s",
-    });
-    setButStyle({ opacity: "0", transition: "0.5s" });
-  };
-
   return (
-    <div className="front_div">
-      <button className="pro_but" style={butStyle} onClick={() => proClick()}>
-        교수용
-      </button>
-      <button className="stu_but" style={butStyle} onClick={() => stuClick()}>
-        학생용
-      </button>
-      <div className="pro_div"></div>
-      <div className="stu_div" style={stuStyle}></div>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/test" element={<Student />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
