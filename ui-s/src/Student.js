3d1ac7c9da60ccfc624c5b45d5320e409a1f32a9
seobkeom <ajm153123@naver.com>
2022-09-22 19:45:18 +0900

    #82 : Feat : FrontPage, StudentPage Update

--- ui-s/src/Student.js
+++ ui-s/src/Student.js
@@ -1,24 +1,79 @@
-import { positions } from '@mui/system'
-import React from 'react'
+import { useState } from 'react'
+
+import { TextField, InputAdornment } from '@mui/material'
+import Box from '@mui/material/Box'
+import SearchIcon from '@mui/icons-material/Search'
+
+import { keyframes } from '@emotion/react'
+
+const searchMove1 = keyframes`
+  from {left: 20vw; top: 45vh; width: 60%; }
+  to {left: 2vw; top: 2vh; width: 30%; }`
+const searchMove2 = keyframes`
+from {width: 90%; px: 2.9; py: 2;}
+to {width: 80%; px: 1; py: 1;}`
 
 function Student() {
+  const [change, setChange] = useState(false)
+
   return (
     <div
       style={{
-        backgroundColor: 'blueviolet',
+        backgroundColor: 'greenyellow',
         height: '100vh',
       }}
     >
-      <div
-        className="test"
-        style={{
-          fontSize: '100px',
-          position: 'absolute',
-          left: '40%',
-          top: '40%',
-        }}
-      >
-        test
+      <div className="test">
+        <Box
+          sx={Object.assign(
+            change
+              ? {
+                  left: '2vw',
+                  top: '2vh',
+                  width: '30%',
+                  animation: `${searchMove1} 1s`,
+                }
+              : {
+                  left: '20vw',
+                  top: '45vh',
+                  width: '60%',
+                },
+            {
+              position: 'absolute',
+              backgroundColor: '#F2F2F2',
+              borderRadius: 25,
+              textAlign: 'center',
+              boxShadow: '2px 8px 20px -12px #bdbdbd',
+            }
+          )}
+        >
+          <TextField
+            onKeyPress={(e) => {
+              setChange(true)
+            }}
+            variant="standard"
+            id="User_ID"
+            placeholder="Solved.ac ID 입력"
+            sx={
+              change
+                ? { width: '80%', px: 1, py: 1, animation: `${searchMove2} 1s` }
+                : { width: '90%', px: 2.9, py: 2 }
+            }
+            // value={userId}
+            // onChange={(e) => setUserId(e.target.value)}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <SearchIcon
+                    onClick={() => {
+                      setChange(true)
+                    }}
+                  />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
       </div>
     </div>
   )
