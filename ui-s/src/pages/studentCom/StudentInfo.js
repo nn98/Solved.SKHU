a1ba2557163aa9f2ceea181bdca598ccaacb00a4
hyunboklee <gusqhrzkq@naver.com>
2022-09-28 20:17:34 +0900

    #82 HOTFIX : 오류 수정

--- ui-s/src/pages/studentCom/StudentInfo.js
+++ ui-s/src/pages/studentCom/StudentInfo.js
@@ -1,70 +1,70 @@
-import React, { useState } from "react";
-import MaxWidthDialog from "./MaxWidthDialog";
-import ContentCard from "./ContentCard";
-import Slide from "@mui/material/Slide";
-import { useEffect } from "react";
+import React, { useState } from 'react'
+import MaxWidthDialog from './MaxWidthDialog'
+import ContentCard from './ContentCard'
+import Slide from '@mui/material/Slide'
+import { useEffect } from 'react'
 
 function StudentInfo(props) {
-  const [userOpen, setUserOpen] = useState(false);
-  const [algoOpen, setAlgoOpen] = useState(false);
-  const [recoOpen, setRecoOpen] = useState(false);
-  const [rankOpen, setRankOpen] = useState(false);
-  const [randOpen, setRandOpen] = useState(false);
-  const [qnaOpen, setQnAOpen] = useState(false);
-  const [slide, setSlide] = React.useState(false);
+  const [userOpen, setUserOpen] = useState(false)
+  const [algoOpen, setAlgoOpen] = useState(false)
+  const [recoOpen, setRecoOpen] = useState(false)
+  const [rankOpen, setRankOpen] = useState(false)
+  const [randOpen, setRandOpen] = useState(false)
+  const [qnaOpen, setQnAOpen] = useState(false)
+  const [slide, setSlide] = React.useState(false)
 
   useEffect(() => {
     setTimeout(function () {
-      setSlide(true);
-    }, 1000);
-  }, []);
+      setSlide(true)
+    }, 1000)
+  }, [])
 
   return (
     <>
       <Slide direction="up" in={slide} timeout={1000}>
         <div
           style={{
-            display: "inline-block",
-            marginLeft: "30vw",
-            marginTop: "2vh",
+            display: 'inline-block',
+            marginLeft: '30vw',
+            marginTop: '2vh',
           }}
         >
           <ContentCard
-            title={"USER"}
-            text={"내 정보 확인"}
-            height={"46vh"}
-            width={"46vw"}
+            title={'USER'}
+            text={'내 정보 확인'}
+            height={'46vh'}
+            width={'46vw'}
             open={userOpen}
             setOpen={setUserOpen}
           ></ContentCard>
         </div>
       </Slide>
       <MaxWidthDialog
-        title={"USER"}
+        title={'USER'}
         open={userOpen}
         setOpen={setUserOpen}
       ></MaxWidthDialog>
-      <div style={{ display: "inline-grid" }}>
+      <div style={{ display: 'inline-grid' }}>
         <Slide direction="up" in={slide} timeout={1000}>
           <div
             style={{
-              display: "inline-block",
-              marginLeft: "1.5vw",
-              marginTop: "2vh",
+              display: 'inline-block',
+              marginLeft: '1.5vw',
+              marginTop: '2vh',
             }}
           >
             <ContentCard
-              title={"SKHU Algorthme"}
-              text={"성공회대 추천 알고리즘"}
-              height={"21.75vh"}
-              width={"21vw"}
+              title={'SKHU Algorthme'}
+              text={'성공회대 추천 알고리즘'}
+              height={'21.75vh'}
+              width={'21vw'}
               open={algoOpen}
               setOpen={setAlgoOpen}
             ></ContentCard>
           </div>
         </Slide>
         <MaxWidthDialog
-          title={"SKHU Algorthme"}
+          title={'SKHU Algorthme'}
           open={algoOpen}
           setOpen={setAlgoOpen}
         ></MaxWidthDialog>
@@ -72,16 +72,16 @@ function StudentInfo(props) {
         <Slide direction="up" in={slide} timeout={2000}>
           <div
             style={{
-              display: "inline-block",
-              marginLeft: "1.5vw",
-              marginTop: "2vh",
+              display: 'inline-block',
+              marginLeft: '1.5vw',
+              marginTop: '2vh',
             }}
           >
             <ContentCard
-              title={"Recommand"}
-              text={"사용자 추천 알고리즘"}
-              height={"21.75vh"}
-              width={"21vw"}
+              title={'Recommand'}
+              text={'사용자 추천 알고리즘'}
+              height={'21.75vh'}
+              width={'21vw'}
               open={recoOpen}
               setOpen={setRecoOpen}
             ></ContentCard>
@@ -92,16 +92,16 @@ function StudentInfo(props) {
       <Slide direction="up" in={slide} timeout={2500}>
         <div
           style={{
-            display: "inline-block",
-            marginLeft: "57vw",
-            marginTop: "2vh",
+            display: 'inline-block',
+            marginLeft: '57vw',
+            marginTop: '2vh',
           }}
         >
           <ContentCard
-            title={"Ranking"}
-            text={"성공회대 구성원 랭킹"}
-            height={"21vh"}
-            width={"27vw"}
+            title={'Ranking'}
+            text={'성공회대 구성원 랭킹'}
+            height={'21vh'}
+            width={'27vw'}
             open={rankOpen}
             setOpen={setRankOpen}
           ></ContentCard>
@@ -112,16 +112,16 @@ function StudentInfo(props) {
       <Slide direction="up" in={slide} timeout={2500}>
         <div
           style={{
-            display: "inline-block",
-            marginLeft: "1.5vw",
-            marginTop: "2vh",
+            display: 'inline-block',
+            marginLeft: '1.5vw',
+            marginTop: '2vh',
           }}
         >
           <ContentCard
-            title={"Random"}
-            text={"백준 문제 랜덤 뽑기"}
-            height={"21vh"}
-            width={"13vw"}
+            title={'Random'}
+            text={'백준 문제 랜덤 뽑기'}
+            height={'21vh'}
+            width={'13vw'}
             open={randOpen}
             setOpen={setRandOpen}
           ></ContentCard>
@@ -132,16 +132,16 @@ function StudentInfo(props) {
       <Slide direction="up" in={slide} timeout={2750}>
         <div
           style={{
-            display: "inline-block",
-            marginLeft: "48.5vw",
-            marginTop: "2vh",
+            display: 'inline-block',
+            marginLeft: '48.5vw',
+            marginTop: '2vh',
           }}
         >
           <ContentCard
-            title={"QnA"}
-            text={"익명게시판"}
-            height={"25vh"}
-            width={"50vw"}
+            title={'QnA'}
+            text={'익명게시판'}
+            height={'25vh'}
+            width={'50vw'}
             open={qnaOpen}
             setOpen={setQnAOpen}
           ></ContentCard>
@@ -149,7 +149,7 @@ function StudentInfo(props) {
       </Slide>
       <MaxWidthDialog open={qnaOpen} setOpen={setQnAOpen}></MaxWidthDialog>
     </>
-  );
+  )
 }
 
-export default StudentInfo;
+export default StudentInfo
