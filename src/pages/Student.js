6bec9d3791853c141bd1c2767100a1e1a0332813
nn98 <jkllhgb@gmail.com>
2022-10-05 00:49:55 +0900

    #82 Feat|Style : spcify searchUser | set searchUser div/tr

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,28 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { TextField, InputAdornment } from '@mui/material'
-import Box from '@mui/material/Box'
-import SearchIcon from '@mui/icons-material/Search'
+import { TextField, InputAdornment } from "@mui/material";
+import Box from "@mui/material/Box";
+import SearchIcon from "@mui/icons-material/Search";
 
-import { keyframes } from '@emotion/react'
+import { keyframes } from "@emotion/react";
 
-import StudentInfo from './studentCom/StudentInfo'
+import StudentInfo from "./studentCom/StudentInfo";
+import Ranking from "./studentCom/rankCom/rank";
 
-import stbg from './studentCom/userCom/image/STBG.png'
+import stbg from "./studentCom/userCom/image/STBG.png";
 
 const searchMove1 = keyframes`
   from {left: 20vw; top: 45vh; width: 60%; }
-  to {left: 2vw; top: 2vh; width: 20%; }`
+  to {left: 2vw; top: 2vh; width: 20%; }`;
 const searchMove2 = keyframes`
 from {width: 90%; px: 2.9; py: 2;}
-to {width: 80%; px: 1; py: 2;}`
+to {width: 80%; px: 1; py: 2;}`;
 const fadein = keyframes`
 from {opacity: 0; }
-to {opacity: 1; }`
+to {opacity: 1; }`;
 
 function Student(props) {
-  const [change, setChange] = useState(false)
-  const [userName, setUserName] = useState('')
+  const [change, setChange] = useState(false);
+  const [userName, setUserName] = useState("");
 
   return (
     <div
@@ -30,8 +31,8 @@ function Student(props) {
         // backgroundColor: 'greenyellow',
 
         backgroundImage: `url(${stbg})`,
-        backgroundSize: 'cover',
-        height: '100vh',
+        backgroundSize: "cover",
+        height: "100vh",
       }}
     >
       <div className="test" style={{ animation: `${fadein} 0.5s` }}>
@@ -39,29 +40,29 @@ function Student(props) {
           sx={Object.assign(
             change
               ? {
-                  left: '2vw',
-                  top: '2vh',
-                  width: '20%',
+                  left: "2vw",
+                  top: "2vh",
+                  width: "20%",
                   animation: `${searchMove1} 1s`,
                 }
               : {
-                  left: '20vw',
-                  top: '45vh',
-                  width: '60%',
+                  left: "20vw",
+                  top: "45vh",
+                  width: "60%",
                 },
             {
-              position: 'absolute',
-              backgroundColor: '#F2F2F2',
+              position: "absolute",
+              backgroundColor: "#F2F2F2",
               borderRadius: 25,
-              textAlign: 'center',
-              boxShadow: '2px 8px 20px -12px #bdbdbd',
+              textAlign: "center",
+              boxShadow: "2px 8px 20px -12px #bdbdbd",
             }
           )}
         >
           <TextField
             onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                setChange(true)
+              if (e.key === "Enter") {
+                setChange(true);
               }
             }}
             autoComplete="off"
@@ -70,8 +71,8 @@ function Student(props) {
             placeholder="Solved.ac ID 입력"
             sx={
               change
-                ? { width: '80%', px: 1, py: 1, animation: `${searchMove2} 1s` }
-                : { width: '90%', px: 2.9, py: 2 }
+                ? { width: "80%", px: 1, py: 1, animation: `${searchMove2} 1s` }
+                : { width: "90%", px: 2.9, py: 2 }
             }
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
@@ -80,7 +81,7 @@ function Student(props) {
                 <InputAdornment position="end">
                   <SearchIcon
                     onClick={() => {
-                      setChange(true)
+                      setChange(true);
                     }}
                   />
                 </InputAdornment>
@@ -100,7 +101,7 @@ function Student(props) {
         <></>
       )}
     </div>
-  )
+  );
 }
 
-export default Student
+export default Student;
--- src/pages/studentCom/randomCom/randomPage.js
+++ src/pages/studentCom/randomCom/randomPage.js
@@ -1,82 +1,79 @@
-import React, { useEffect, useState, } from 'react'
-import Lottie from 'lottie-react'
-import randomBox from './image/randomBox.json'
-import BoxEffect from './image/BoxEffect.json'
-import ProCard from '../recoCom/proCard'
+import React, { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+import randomBox from "./image/randomBox.json";
+import BoxEffect from "./image/BoxEffect.json";
+import ProCard from "../recoCom/proCard";
 function RandomPage(props) {
-  const [time, setTime] = useState(0)
-  const [time2, setTime2] = useState(0)
-  useEffect(
-    () =>{
-      setTimeout(() => {
-        setTime(1)
-        console.log('5555')
-      }, 1000)
-      setTimeout(() => {
-        setTime2(1)
-        console.log('5555')
-      }, 600)}
-      ,[]
-  )
+  const [time, setTime] = useState(0);
+  const [time2, setTime2] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(1);
+      console.log("5555");
+    }, 1000);
+    setTimeout(() => {
+      setTime2(1);
+      console.log("5555");
+    }, 600);
+  }, []);
   // var randomNo = (Math.floor)(Math.random()*24726+1000)
   return (
     <div>
       <div
         style={{
-          position: 'relative',
-          width: '100vw',
-          height: '90vh',
-          overflow: 'hidden',
+          position: "relative",
+          // width: '100vw',
+          height: "90vh",
+          overflow: "hidden",
         }}
         onClick={(e) => {
-          props.setOpen(false)
+          props.setOpen(false);
         }}
       >
-        
         <Lottie
           loop={false}
           animationData={randomBox}
           style={{
-            height: '100%',
-            width: '30%',
-            position: 'absolute',
-            left: '30%',
+            height: "100%",
+            width: "30%",
+            position: "absolute",
+            left: "30%",
           }}
         />
         <Lottie
           loop={1}
           animationData={BoxEffect}
           style={{
-            top: '-25vh',
-            left: '10%',
-            width: '70%',
-            position: 'absolute',
+            top: "-25vh",
+            left: "10%",
+            width: "70%",
+            position: "absolute",
             opacity: `${time2}`,
-            transition: 'opacity 0.3s ease 0.3s',
+            transition: "opacity 0.3s ease 0.3s",
           }}
         />
         <div
           style={{
-            position: 'absolute',
-            width: '40%',
-            height: '100%',
-            left: '25%',
+            position: "absolute",
+            width: "40%",
+            height: "100%",
+            left: "25%",
             opacity: `${time}`,
-            transition: 'opacity 0.3s ease 0.3s',
+            transition: "opacity 0.3s ease 0.3s",
           }}
         >
           <ProCard
-            top={'25vh'}
-            fontSize={'2rem'}
-            proTier={'13'}
-            proColor={'#ec9a00'}
-            width={'100%'}
-            height={'40%'}
+            top={"25vh"}
+            fontSize={"2rem"}
+            proTier={"13"}
+            proColor={"#ec9a00"}
+            width={"100%"}
+            height={"40%"}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default RandomPage
+export default RandomPage;
