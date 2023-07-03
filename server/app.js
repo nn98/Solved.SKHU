152baf59159b4bc8fc875c89bcb5b0a15a3c3dd0
nn98 <jkllhgb@gmail.com>
2023-07-04 06:17:03 +0900

    Comment: Uncomment https's

--- server/app.js
+++ server/app.js
@@ -35,32 +35,32 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-// const privateKey = fs.readFileSync(
-//   '/etc/letsencrypt/live/sol-skhu.duckdns.org/privkey.pem',
-//   'utf8'
-// );
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/sol-skhu.duckdns.org/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/sol-skhu.duckdns.org/chain.pem', 'utf8');
-//
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca,
-// };
-//
-// // Starting both http & https servers
-// const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
-//
-// /*
-// httpServer.listen(80, () => {
-//   console.log('HTTP Server running on port 80');
-// });
-// */
-//
-// httpsServer.listen(httpsport, () => {
-//   console.log(`HTTPS Server running on port  ${httpsport}`);
-// });
+const privateKey = fs.readFileSync(
+  '/etc/letsencrypt/live/sol-skhu.duckdns.org/privkey.pem',
+  'utf8'
+);
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sol-skhu.duckdns.org/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/sol-skhu.duckdns.org/chain.pem', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+/*
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+});
+*/
+
+httpsServer.listen(httpsport, () => {
+  console.log(`HTTPS Server running on port  ${httpsport}`);
+});
 
 app.get('/', (req, res) => {
   res.send('working?');
