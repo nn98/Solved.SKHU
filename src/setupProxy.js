86cda2baf754ea37bbff06bcb2b11d2fc9d479c6
nn98 <jkllhgb@gmail.com>
2023-08-01 06:26:47 +0900

    Comment: Clear past

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,6 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
       target: 'https://solved.ac',
-      // target: 'http://localhost:3000',
-      // pathRewrite: {
-      //     '^/api': '',
-      // },
       changeOrigin: true,
     }),
   );
