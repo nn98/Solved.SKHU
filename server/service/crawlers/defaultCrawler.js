365045934f34ca8dedd47770cbfedb9853cab548
nn98 <jkllhgb@gmail.com>
2025-04-21 14:44:20 +0900

    refactor: Separate crawler by service

---
+++ server/service/crawlers/defaultCrawler.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
+class CrawlerService {
+    static async fetchPage(url) {
+        const browser = await puppeteer.launch({ headless: true });
+        try {
+            const page = await browser.newPage();
+            await page.goto(url, { waitUntil: 'networkidle2' });
+            const content = await page.content();
+            return cheerio.load(content); // $ 반환
+        } finally {
+            await browser.close();
+        }
+    }
+}
+module.exports = CrawlerService;
